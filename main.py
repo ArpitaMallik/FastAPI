bea376195be7c7b6f2499755ce41d4b322ab9f20
Arpita Mallik <arpitamallik13@gmail.com>
2025-06-23 20:58:37 +0600

    updated main

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ app = FastAPI()
 
 @app.get('/') #create a route that listens to the get method
 def hello():
-    return ('message':"Hello World!")
+    return {'message':"Hello World!"}
+
+
+@app.get('/about')
+def about():
+    return {'message': 'I am Arpita Mallik and I am interested in AI and ML.'}
