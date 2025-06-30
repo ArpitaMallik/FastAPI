c0533a73c21967cd7e343e563e12ee1c47e43d7d
Arpita Mallik <arpitamallik13@gmail.com>
2025-07-01 01:14:58 +0600

    added json

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open('patient.json', 'r') as f:
+        data = json.load(f)
+
+    return data
+
 @app.get('/') #create a route that listens to the get method
 def hello():
-    return {'message':"Hello World!"}
+    return {'message':"Patient Management System API"}
 
 
 @app.get('/about')
 def about():
-    return {'message': 'I am Arpita Mallik and I am interested in AI and ML.'}
+    return {'message': 'A fully functional API to manage your patient records'}
+
+@app.get('/view')
+def view():
+    data = load_data()
+
+    return data
