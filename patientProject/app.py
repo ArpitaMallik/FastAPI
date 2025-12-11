7db95cf1b62f1c7efbef4f5d5e3ecc743e3ee05b
Arpita Mallik <arpitamallik13@gmail.com>
2025-12-11 14:49:00 +0600

    types of apis done

--- patientProject/app.py
+++ patientProject/app.py
@@ -1,18 +1,15 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from typing import Literal, Annotated
 import pandas as pd
 from schema.user_input import UserInput
-from config.city_tier import tier_1_cities, tier_2_cities
-
+from model.predict import predict_output, MODEL_VERSION, model
+from schema.pred_response import PredictionResponse
 
 app = FastAPI()
-    
 
 @app.get('/')
 def home():
     return {"message": "Welcome to the Insurance Premium Category Predictor API!"}
-
 
 @app.get('/health')
 def health_check():
@@ -22,32 +19,32 @@ def health_check():
         'model_loaded': model is not None
     }
 
-
-
-
-@app.post('/predict')
+@app.post('/predict', response_model=PredictionResponse)
 def predict_premium(data: UserInput):
 
-    input_df = pd.DataFrame([{
+    user_input = {
         'bmi': data.bmi,
         'age_group': data.age_group,
         'lifestyle_risk': data.lifestyle_risk,
         'city_tier': data.city_tier,
         'income_lpa': data.income_lpa,
         'occupation': data.occupation
-    }])
+    }
 
-    # Prediction
-    predicted_class = model.predict(input_df)[0]
+    # Wrap in a list so DataFrame is shaped correctly for predict.py
+    user_input_list = [user_input]
 
-    # Probabilities for each class
+    # Get prediction
+    predicted_class = predict_output(user_input_list)
+
+    # Calculate probabilities if supported
     if hasattr(model, "predict_proba"):
+        input_df = pd.DataFrame(user_input_list)
         probabilities = model.predict_proba(input_df)[0]
         class_labels = model.classes_
         class_probabilities = dict(zip(class_labels, probabilities))
         confidence = float(class_probabilities[predicted_class])
     else:
-        # If the model doesn't support predict_proba
         class_probabilities = {}
         confidence = None
 
@@ -58,7 +55,3 @@ def predict_premium(data: UserInput):
             "class_probabilities": class_probabilities
         }
     })
-
-
-
-
